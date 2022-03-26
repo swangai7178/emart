21bd82616152f054f9461c1af5430b3b0a71fd57
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-03-26 21:22:28 +0300

    b

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -40,16 +40,24 @@ export default function Products() {
         {filter.map((product) => {
           return (
             <>
-              <div className="col-md-3">
-                <div class="card">
-                  <img src={product.image} class="card-img-top" alt={product.title} />
-                  <div class="card-body">
-                    <h5 class="card-title">{product.title}</h5>
-                    <p class="card-text">
-                      ${product.price}
-                    </p>
-                    <a href="" class="btn btn-primary">
-                      Go somewhere
+              <div className="col-md-3 mb-4">
+                <div className="card h-100 text-center p-5 " key={product.id}>
+                  <img
+                    src={product.image}
+                    class="card-img-top"
+                    alt={product.title}
+                    height="250px"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title mb = 0">
+                      {product.title.substring(0, 12)}
+                    </h5>
+                    <p className="card-text lead fw-bold">${product.price}</p>
+                    <a
+                      href="#3"
+                      className="btn btn-outline-dark fa fa-shopping-cart me-1"
+                    >
+                          buy
                     </a>
                   </div>
                 </div>
@@ -65,12 +73,12 @@ export default function Products() {
       <div className="container my-5 py-5">
         <div className="row">
           <div className="col-12 mb-5">
-            <h1 className="display-6 fw-bolder text-center">Latest products</h1>
+            <h1 className="display-3 fw-bolder text-center">Latest products</h1>
             <hr />
           </div>
         </div>
-        <div className="row justify-content-center"></div>
-        {loading ? <Loading /> : <ShowProduct />}
+        <div className="row"></div>
+        {loading? <Loading /> : <ShowProduct />}
       </div>
     </div>
   );
